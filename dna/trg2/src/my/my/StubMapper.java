eb462e729123d99e5a27880cc6f9f6427ede4672
Anup <rtre84@live.com>
2016-12-13 23:21:53 -0500

    Update StubMapper.java

--- dna/trg2/src/my/my/StubMapper.java
+++ dna/trg2/src/my/my/StubMapper.java
@@ -10,44 +10,6 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.io.Writable;
 import org.apache.hadoop.mapreduce.Mapper;
 
-//public class StubMapper extends Mapper<Object, Text, Text, LongWritable> {
-//
-//  @Override
-//  public void map(Object key, Text value, Context context)
-//      throws IOException, InterruptedException {
-//    /*
-//     * TODO implement
-//     */
-//	  String[] words = value.toString().split("[ \t]+");
-//	  for(String word:words)
-//	  {
-//		  System.out.println("ddd");
-//		  System.err.println("erorr");
-//		  
-//		  context.write(new Text(word), new LongWritable(1));
-//	  }
-//  }
-//}
-
-
-//public class StubMapper extends Mapper<LongWritable, Text, Text, TextArrayWritable> {
-//	private final static TextArrayWritable writable = new TextArrayWritable();
-//	private final static Writable[] writables = new Writable[1];
-//	private Text newKey = new Text();
-//
-//	@Override
-//	public void map(LongWritable key, Text value, Context context)
-//			throws IOException, InterruptedException {
-//		String word = value.toString();
-//		char[] chars = word.toCharArray();
-//		Arrays.sort(chars);
-//		newKey.set(new String(chars));
-//		writables[0] = new Text(word);
-//		writable.set(writables);
-//		context.write(newKey, writable);
-//	}
-//}
-
 
 public class StubMapper  extends Mapper <Object, Text, Text, Text> {
 	
@@ -59,10 +21,6 @@ public class StubMapper  extends Mapper <Object, Text, Text, Text> {
 			 String newKey = null;
 
 			 newKey = dNASeq1[1];
-//			 if (dNASeq1.compareTo(dNASeq2) ==-1) {
-//				newKey = dNASeq1;
-//			 }else
-//				 newKey = dNASeq2;
 				 
 			 context.write(new Text(newKey), new Text(dNASeq1[0]));
 		}
